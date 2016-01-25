f40ddb8389c0e76b8265fee5d0a2a6b000f5bb66
Sephir <doekasu@hotmail.com>
2016-01-25 17:57:57 -0500

    added functionality
    TODO:
    better pie chart load
    alternative way of loading information

--- Consults/consulta_distribuido.php
+++ Consults/consulta_distribuido.php
@@ -42,10 +42,10 @@
 		  	    <div class="col-xs-12col-sm-4 col-md-4 col-lg-4">
 		  	      <div class="row center-block">
 		  	        <div class="col-xs-6 col-sm-6 col-md-6 col-lg-6">
-		  	          <button id="mistery" type="button" class="btn btn-block center-block btn-info">Colombia</button>
+		  	          <button id="btnColombia" type="button" class="btn btn-block center-block btn-info">Colombia</button>
 		  	        </div>
-		  	        <div id="adventure" class="col-xs-6 col-sm-6 col-md-6 col-lg-6">
-		  	          <button type="button" class="btn btn-block center-block btn-info">Argentina</button>
+		  	        <div class="col-xs-6 col-sm-6 col-md-6 col-lg-6">
+		  	          <button id="btnArgentina" type="button" class="btn btn-block center-block btn-info">Argentina</button>
 		  	        </div>
 		  	      </div>
 		  	    </div>
@@ -67,120 +67,169 @@
 		  	</div>
 	  	</div>
 		<!-- Modal -->
-	    <div class="modal fade" id="CundinamarcaModal" tabindex="-1" role="dialog" aria-labelledby="myModalLabel">
+	    <div class="modal fade" id="pieModal" tabindex="-1" role="dialog" aria-labelledby="myModalLabel">
 	      <div class="modal-dialog modal-lg" role="document">
 	        <div class="modal-content">
 	          <div class="modal-header">
 	            <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
 	            <h4 class="modal-title" id="myModalLabel">Ciudad Seleccionada</h4>
 	          </div>
-	          <div class="modal-body" id="CundinamarcaModalBody">
+	          <div class="modal-body" id="ModalBody">
 	            
 	          </div>
+	          <?php
+				include '../footer.php';
+			  ?>
 		<script src="http://localhost:8080/informe_ventas/JS/d3.min.js" charset="utf-8"></script>
 		<script type="text/javascript">
-			d3.csv('http://localhost:8080/informe_ventas/Data/colombia.csv', function(data){
-				var container = d3.select("#reportTable")
-	                .append("table")
-	                .attr('class', 'table-hover');
-			    columnsName = Object.keys( data[0] ); 
-				// create table header
-			    container.append('thead').append('tr')
-			        .selectAll('th')
-			        .data(columnsName).enter()
-			        .append('th')
-			        .text(function(d) {
-				        return d;
-				    });
-				var tr = container.append('tbody').selectAll('tr')
-				    .data(data).enter()
-				    .append('tr');
+			function loadCountryInformation(countryName){
+				d3.csv('http://localhost:8080/informe_ventas/Data/' + countryName + '.csv', function(data){
+					var container = d3.select("#reportTable")
+		                .append("table")
+		                .attr('class', 'table-hover');
+				    columnsName = Object.keys( data[0] ); 
+					// create table header
+				    container.append('thead').append('tr')
+				        .selectAll('th')
+				        .data(columnsName).enter()
+				        .append('th')
+				        .text(function(d) {
+					        return d;
+					    });
+					var tr = container.append('tbody').selectAll('tr')
+					    .data(data).enter()
+					    .append('tr');
 
-				tr.append('th')
-				    .attr('class', 'Ciudad')
-				    .html(function(m) { return "<a data-toggle='modal' data-target='#" + m.Ciudad + "Modal'>" + m.Ciudad + "</a>"; });
+					tr.append('th')
+					    .attr('class', 'Ciudad')
+					    .html(function(m) { return "<a onClick='getCityProducts();' id='" + countryName + "|" + m.Ciudad +"'>" + m.Ciudad + "</a>"; });
 
-				tr.append('td')
-				    .attr('class', 'Porcentaje')
-				    .html(function(m) { return m.Porcentaje; });
+					tr.append('td')
+					    .attr('class', 'Porcentaje')
+					    .html(function(m) { return m.Porcentaje; });
 
-				tr.append('td')
-				    .attr('class', 'Valor Bruto')
-				    .html(function(m) { return m.ValorBruto; });
+					tr.append('td')
+					    .attr('class', 'Valor Bruto')
+					    .html(function(m) { return m.ValorBruto; });
 
-				tr.append('td')
-				    .attr('class', 'IVA')
-				    .html(function(m) { return m.IVA; });
+					tr.append('td')
+					    .attr('class', 'IVA')
+					    .html(function(m) { return m.IVA; });
 
-				tr.append('td')
-				    .attr('class', 'Descuento')
-				    .html(function(m) { return m.Descuento; });
+					tr.append('td')
+					    .attr('class', 'Descuento')
+					    .html(function(m) { return m.Descuento; });
 
-				tr.append('td')
-				    .attr('class', 'VentaNeta')
-				    .html(function(m) { return m.VentaNeta; });
+					tr.append('td')
+					    .attr('class', 'VentaNeta')
+					    .html(function(m) { return m.VentaNeta; });
+				});
+			}
+
+			$(document).ready(function() {
+				$("#btnColombia").click(function() {
+					$(".jumbotron").slideUp("slow",function() {
+						$("#ModalBody svg").remove();
+						$("#reportTable table").remove();
+						loadCountryInformation("Colombia");
+					});
+					$(".jumbotron").slideDown("slow");
+					
+				});
+
+				$("#btnArgentina").click(function() {
+					$(".jumbotron").slideUp("slow",function() {
+						$("#ModalBody svg").remove();
+						$("#reportTable table").remove();
+						loadCountryInformation("Argentina");
+					});
+					$(".jumbotron").slideDown("slow");
+				});
+
 			});
 
-			var width = 900,
-			    height = 500,
-			    radius = Math.min(width, height) / 2;
+			function getCityProducts(){
+				$("#ModalBody svg").remove();
+				$('#pieModal').modal('show');
+		        generateModalPieChart(event.target.id);
+			}
 
-			var color = d3.scale.ordinal()
-			    .range(["#98abc5", "#8a89a6", "#7b6888", "#6b486b", "#a05d56", "#d0743c", "#ff8c00"]);
+			function generateModalPieChart(countryCity){
+				var countryCityArray = countryCity.split("|");
+				var country = countryCityArray[0];
+				var city = countryCityArray[1];
+				var noSpaceCity = city.replace(/\s/g, "_");
+				$("#myModalLabel").html(city + ":");
+				var width = 900,
+				    height = 500,
+				    radius = Math.min(width, height) / 2;
 
-			var arc = d3.svg.arc()
-			    .outerRadius(radius - 10)
-			    .innerRadius(0);
+				var color = d3.scale.ordinal()
+				    .range(["#98abc5", "#8a89a6", "#7b6888", "#6b486b", "#a05d56", "#d0743c", "#ff8c00"]);
 
-			var labelArc = d3.svg.arc()
-			    .outerRadius(radius - 40)
-			    .innerRadius(radius - 40);
+				var arc = d3.svg.arc()
+				    .outerRadius(radius - 10)
+				    .innerRadius(0);
 
-			var pie = d3.layout.pie()
-			    .sort(null)
-			    .value(function(d) { return d.Porcentaje; });
+				var labelArc = d3.svg.arc()
+				    .outerRadius(radius - 40)
+				    .innerRadius(radius - 40);
 
-			var svg = d3.select("#CundinamarcaModalBody").append("svg")
-				.attr('id', 'cundinamarcaSVG')
-			    .attr("width", width)
-			    .attr("height", height)
-			    .attr('viewBox', '0 0 ' + width + ' ' + height)
-			  	.attr('preserveAspectRatio', 'xMidYMid  meet')
-			  .append("g")
-			    .attr("transform", "translate(" + width / 2 + "," + height / 2 + ")");
+				var pie = d3.layout.pie()
+				    .sort(null)
+				    .value(function(d) { return d.Porcentaje; });
 
-			d3.csv("http://localhost:8080/informe_ventas/Data/colombia_cundinamarca.csv", type, function(error, data) {
-			  if (error) throw error;
+				var svg = d3.select("#ModalBody").append("svg")
+					.attr('id', 'countrySVG')
+				    .attr("width", width)
+				    .attr("height", height)
+				    .attr('viewBox', '0 0 ' + width + ' ' + height)
+				  	.attr('preserveAspectRatio', 'xMidYMid  meet')
+				  .append("g")
+				    .attr("transform", "translate(" + width / 2 + "," + height / 2 + ")");
 
-			  var g = svg.selectAll(".arc")
-			      .data(pie(data))
-			    .enter().append("g")
-			      .attr("class", "arc");
+				d3.csv("http://localhost:8080/informe_ventas/Data/" + country + "_" + noSpaceCity + ".csv", type, function(error, data) {
+				  if (error) throw error;
 
-			  g.append("path")
-			      .attr("d", arc)
-			      .style("fill", function(d) { return color(d.data.Producto); });
+				  var g = svg.selectAll(".arc")
+				      .data(pie(data))
+				    .enter().append("g")
+				      .attr("class", "arc");
 
-			  g.append("text")
-			      .attr("transform", function(d) { return "translate(" + labelArc.centroid(d) + ")"; })
-			      .attr("dy", ".35em")
-			      .text(function(d) { return d.data.Producto; });
-			  var chart = $("#cundinamarcaSVG"),
-			      aspect = chart.width() / chart.height(),
-			      mContainer = chart.parent();
-			  $(window).on("resize", function() {
-			      var targetWidth = mContainer.width();
-			      if(targetWidth >= 0)
-			      {
-				        chart.attr("width", targetWidth);
-				        chart.attr("height", Math.round(targetWidth / aspect));
-				  }else
-				  {
-				  		chart.attr("width", 500);
-				        chart.attr("height", 250);
-				  }
-			  }).trigger("resize");
-			});
+				  g.append("path")
+				      .attr("d", arc)
+				      .style("fill", function(d) { return color(d.data.Producto); });
+
+				  g.append("text")
+				      .attr("transform", function(d) { return "translate(" + labelArc.centroid(d) + ")"; })
+				      .attr("dy", ".35em")
+				      .text(function(d) { return d.data.Producto; });
+				  var chart = $("#countrySVG"),
+				      aspect = chart.width() / chart.height(),
+				      mContainer = chart.parent();
+			      $(window).on('shown.bs.modal', function() { 
+			          	var chart = $("#countrySVG"),
+						aspect = chart.width() / chart.height(),
+						mContainer = chart.parent();
+						var targetWidth = mContainer.width();
+						chart.attr("width", targetWidth);
+						chart.attr("height", Math.round(targetWidth / aspect));
+			      });
+				  $(window).on("resize", function() {
+				      var targetWidth = mContainer.width();
+				      if(targetWidth >= 0)
+				      {
+					        chart.attr("width", targetWidth);
+					        chart.attr("height", Math.round(targetWidth / aspect));
+					  }else
+					  {
+					  		chart.attr("width", 500);
+					        chart.attr("height", 250);
+					  }
+				  }).trigger("resize");
+
+				});
+			}
 
 			function type(d) {
 			  d.Porcentaje = +d.Porcentaje;
@@ -188,9 +237,6 @@
 			}
 			
 		</script>
-		<?php
-			include '../footer.php';
-		?>
 		<script src="http://localhost:8080/informe_ventas/JS/jquery.ba-throttle-debounce.min.js" charset="utf-8"></script>
 		<script src="http://localhost:8080/informe_ventas/JS/jquery.stickyheader.js" charset="utf-8"></script>
 		</script>

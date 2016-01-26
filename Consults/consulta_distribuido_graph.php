3b420faafc87a8b84dbbc14e8ca810f3a41baa32
Sephir <doekasu@hotmail.com>
2016-01-26 14:46:47 -0500

    old graphs
    TODO:
    change graph for interactive graph

--- Consults/consulta_distribuido_graph.php
+++ Consults/consulta_distribuido_graph.php
@@ -175,7 +175,8 @@ function generateModalPieChart(countryCity){
 	height = 500,
 	radius = Math.min(width, height) / 2;
 
-	var color = d3.scale.category20b();
+	var color = d3.scale.ordinal()
+	.range(["#98abc5", "#8a89a6", "#7b6888", "#6b486b", "#a05d56", "#d0743c", "#ff8c00"]);
 
 	var arc = d3.svg.arc()
 	.outerRadius(radius - 10)
--- footer.php
+++ footer.php
@@ -1,5 +1,3 @@
-	</div>	
 	<script type='text/javascript' src='http://localhost:8080/informe_ventas/JS/jquery-2.2.0.min.js'></script>
 	<script type='text/javascript' src='http://localhost:8080/informe_ventas/JS/jquery-ui.min.js'></script>
 	<script type='text/javascript' src='http://localhost:8080/informe_ventas/JS/d3-jetpack.js'></script>
-	<script src="http://localhost:8080/informe_ventas/JS/bootstrap.min.js" integrity="sha384-0mSbJDEHialfmuBBQP6A4Qrprq5OVfW37PRR3j5ELqxss1yVqOtnepnHVP9aJ7xS" crossorigin="anonymous"></script>

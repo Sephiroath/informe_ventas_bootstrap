ac808f49f35bba756d5ae17254d4a1e405365707
Sephir <doekasu@hotmail.com>
2016-02-09 17:57:55 -0500

    better colors
    changed colors from the table for better view

--- Consults/consulta_distribuido.php
+++ Consults/consulta_distribuido.php
@@ -100,6 +100,8 @@
 			<div class="modal-body" id="ModalBody">
 				
 			</div>
+			<div id="ProductSelector">
+			</div>
 			<div id="toolTipDiv">
 				<table>
 					<tr>
@@ -123,9 +125,12 @@
 include '../footer.php';
 ?>
 <script src="http://localhost:8080/informe_ventas/JS/d3.min.js" charset="utf-8"></script>
+<script src="http://localhost:8080/informe_ventas/JS/jquery.stickytableheaders.min.js" charset="utf-8"></script>
 <script type="text/javascript">
 	var lastColor;
 	var radius;
+	var smXLegendCounter = 0;
+	var smYLegendCounter = 0;
 	function loadCountryInformation(countryName){
 		d3.csv('http://localhost:8080/informe_ventas/Data/' + countryName + '.csv', function(data){
 			var container = d3.select("#reportTable")
@@ -173,7 +178,9 @@ include '../footer.php';
 
 					tr.append('td')
 					.attr('class', 'VentaNeta')
-					.html(function(m) { return m.VentaNeta; });
+					.html(function(m) { return m.VentaNeta; });	
+					$('#reportTable table').stickyTableHeaders();
+
 				});
 }
 
@@ -181,7 +188,10 @@ $(document).ready(function() {
 	$("#btnColombia").click(function() {
 		$(".jumbotron").slideUp("slow",function() {
 			$("#ModalBody svg").remove();
+			$("#ProductSelector svg").remove();
 			$("#reportTable table").remove();
+			smXLegendCounter = 0;
+			smYLegendCounter = 0;
 			loadCountryInformation("Colombia");
 		});
 		$(".jumbotron").slideDown("slow");
@@ -191,7 +201,10 @@ $(document).ready(function() {
 	$("#btnArgentina").click(function() {
 		$(".jumbotron").slideUp("slow",function() {
 			$("#ModalBody svg").remove();
+			$("#ProductSelector svg").remove();
 			$("#reportTable table").remove();
+			smXLegendCounter = 0;
+			smYLegendCounter = 0;
 			loadCountryInformation("Argentina");
 		});
 		$(".jumbotron").slideDown("slow");
@@ -201,6 +214,9 @@ $(document).ready(function() {
 
 function getCityProducts(){
 	$("#ModalBody svg").remove();
+	$("#ProductSelector svg").remove();
+	smXLegendCounter = 0;
+	smYLegendCounter = 0;
 	$('#pieModal').modal('show');
 	generateModalPieChart(event.target.id);
 }
@@ -214,9 +230,14 @@ function generateModalPieChart(countryCity){
 	var width = 900,
 	height = 500,
 	radius = Math.min(width, height) / 2;
+	var widthLegendsm = 400,
+	heightLegendsm = 200;
+	
 	var donutWidth = 75;
     var legendRectSize = 18;
+    var smlegendRectSize = 20;
     var legendSpacing = 4;
+    var smlegendSpacing = 4;
 
 	var color = d3.scale.category20b();
 
@@ -295,22 +316,85 @@ function generateModalPieChart(countryCity){
 			$('#quantityLabel').html("");
 			$('#percentageLabel').html("");
 		});
+		//jjjjj
+		var svg2 = d3.select("#ProductSelector").append("svg")
+			.attr('id', 'SVGProductSelector')
+			.attr('class', ' hidden-sm hidden-lg')
+			.attr("width", widthLegendsm)
+			.attr("height", heightLegendsm)
+			.attr('viewBox', '0 0 ' + widthLegendsm + ' ' + heightLegendsm)
+			.attr('preserveAspectRatio', 'xMidYMid  meet')
+			.append("g")
+			.attr("transform", "translate(" + 50 + "," + 50 + ")");
+		var smlegend = svg2.selectAll('.smlegend')
+			.data(color.domain())
+			.enter()
+			.append('g')
+			.attr('class', 'smlegend')
+			.attr('transform', function(d, i) {
+			  var height = smlegendRectSize + smlegendSpacing;
+			  var offset =  height * color.domain().length / 2;
+			  if (smXLegendCounter >= 3)
+			  {
+			  	smXLegendCounter = 0;
+			  	smYLegendCounter++;
+			  }
+			  var horz = (smXLegendCounter * 120);
+			  smXLegendCounter++;
+			  var vert = smYLegendCounter * height;
+			  return 'translate(' + horz + ',' + vert + ')';
+			});
+			smlegend.append('rect')
+			.attr('width', smlegendRectSize)
+			.attr('height', smlegendRectSize)                                   
+			.style('fill', color)
+			.style('stroke', color)
+			.on('click', function(label) {
+			  var rect = d3.select(this);
+			  var enabled = true;
+			  var totalEnabled = d3.sum(data.map(function(d) {     
+			    return (d.enabled) ? 1 : 0;                           
+			  }));
+			  
+			  if (rect.attr('class') === 'disabled') {                
+			    rect.attr('class', '');                               
+			  } else {                                                
+			    if (totalEnabled < 2) return;                         
+			    rect.attr('class', 'disabled');                       
+			    enabled = false;                                      
+			  }
 
-		/* OPTIONAL 
-		path.on('mousemove', function(d) {
-		tooltip.style('top', (d3.event.pageY + 10) + 'px')
-		  .style('left', (d3.event.pageX + 10) + 'px');
-		});
-		*/
+			  pie.value(function(d) {                                 
+			    if (d.label === label) d.enabled = enabled;           
+			    return (d.enabled) ? d.count : 0;                     
+			  });                                                     
 
+			  path = path.data(pie(data));                         
+
+			  path.transition()                                       
+			    .duration(750)                                        
+			    .attrTween('d', function(d) {                         
+			      var interpolate = d3.interpolate(this._current, d); 
+			      this._current = interpolate(0);                     
+			      return function(t) {                                
+			        return arc(interpolate(t));                       
+			      };                                                  
+			    });                                                   
+			});
+			smlegend.append('text')
+			.attr('x', smlegendRectSize + smlegendSpacing)
+			.attr('y', smlegendRectSize - smlegendSpacing)
+			.text(function(d) { return d; });
+
+		//jjjjj
 		var legend = svg.selectAll('.legend')
 			.data(color.domain())
 			.enter()
 			.append('g')
-			.attr('class', 'legend')
+			.attr('class', 'legend hidden-xs')
 			.attr('transform', function(d, i) {
 			  var height = legendRectSize + legendSpacing;
-			  var offset =  height * color.domain().length / 2;
+			  var offset =  height * color.domain().length / 2;			  
 			  var horz = -2 * legendRectSize;
 			  var vert = i * height - offset;
 			  return 'translate(' + horz + ',' + vert + ')';
@@ -360,12 +444,16 @@ function generateModalPieChart(countryCity){
 		.text(function(d) { return d; });
 
 		var chart = $("#countrySVG"),
+		chart2 = $("#SVGProductSelector"),
 		aspect = chart.width() / chart.height(),
+		aspect2 = chart2.width() / chart2.height(),
 		mContainer = chart.parent();
 		$(window).on('shown.bs.modal', function() { 
 			var targetWidth = mContainer.width();
 			chart.attr("width", targetWidth);
 			chart.attr("height", Math.round(targetWidth / aspect));
+			chart2.attr("width", targetWidth);
+			chart2.attr("height", Math.round(targetWidth / aspect2));
 		});
 		$(window).on("resize", function() {
 			var targetWidth = mContainer.width();
@@ -373,10 +461,14 @@ function generateModalPieChart(countryCity){
 			{
 				chart.attr("width", targetWidth);
 				chart.attr("height", Math.round(targetWidth / aspect));
+				chart2.attr("width", targetWidth);
+				chart2.attr("height", Math.round(targetWidth / aspect2));
 			}else
 			{
 				chart.attr("width", 500);
 				chart.attr("height", 250);
+				chart2.attr("width", 200);
+				chart2.attr("height", 200);
 			}
 		}).trigger("resize");
 
--- header.php
+++ header.php
@@ -14,15 +14,15 @@
             <!-- Collect the nav links, forms, and other content for toggling -->
             <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
               <ul class="nav navbar-nav navbar-right">
-                <li><a href="#">Link</a></li>
+                <li><a href="#">Inicio</a></li>
                 <li class="dropdown">
-                  <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Dropdown <span class="caret"></span></a>
+                  <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Opciones <span class="caret"></span></a>
                   <ul class="dropdown-menu">
-                    <li><a href="#">Action</a></li>
-                    <li><a href="#">Another action</a></li>
-                    <li><a href="#">Something else here</a></li>
+                    <li><a href="#">Ingresar</a></li>
+                    <li><a href="#">Registrar</a></li>
+                    <li><a href="#">(otra opcion)</a></li>
                     <li role="separator" class="divider"></li>
-                    <li><a href="#">Separated link</a></li>
+                    <li><a href="#">Salir</a></li>
                   </ul>
                 </li>
               </ul>
